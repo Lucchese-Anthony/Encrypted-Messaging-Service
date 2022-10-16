dec07f8788a5c4c70084dc86376715bf18fa262e
Fink9292 <115906428+Fink9292@users.noreply.github.com>
2022-10-15 22:03:30 -0400

    updating client

--- client.py
+++ client.py
@@ -5,13 +5,31 @@ import socket
 import os
 
 def main():
-    host = "127.0.0.1"
+    host = ""
+    with open('ip.txt', 'r') as file:
+        host = file.read().rstrip()
     port = 9001
 
     client = socket.create_connection(address=(host, port))
     client.sendall(b'Hello, world')
     response = client.recv(1024)
     print('Received', repr(response))
+    return
+
+def incomingMessageThread():
+    # TODO incoming threads
+    return
+
+def outgoingMesssageThread():
+    # TODO outgoing threads
+    return
+
+def MessageEncryption():
+    # TODO Message Encryption
+    return
+
+def MessageDecryption():
+    # TODO Message Decryption
     return
 
 if __name__ == "__main__":
